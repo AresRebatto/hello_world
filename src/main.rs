d6a6b1e698ed3136d68a9b71a8542b31de1d2375
AresRebatto <aresrebatt@gmail.com>
2023-06-19 16:36:48 +0200

    Tuple, Stringhe, Heap, Stack, Ownership ecc.

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,19 @@ fn main() {
     let x: i32 = 5; //La possibilità di creare una variabile con lo stesso nome di un'altra precedente variabile è detta Shadowing: si dice 
     //che la seconda variabile ha messo in ombra (shadowed) la prima: quando andremo quindi a richiarmare la variabile x, avrà valore 5.
     //ATTENZIONE. Quello che abbiamo fatto non è stato cambiare il valore della variabile, ma sovrascrivere l'intera variabile con una nuova
+    
+    let tup: (i32, f64) = (4, 8.09); //Tuple composto da un intero a 32 bit e un float a 64
+
+    let primoElemento: i32 = tup.0; //Memorizza in una variabile l'elemento 0 del Tuple(4)
+    println!("{primoElemento}");
+    
     if n < 100
     {
         while n < 100 
         {
             n = n+1;
         }
-        /*Per un ciclo infinito si utilizza la keyword loop al posto del while e non si mette nessuna condizione nel seguente modo:
+        /*Per un ciclo infinito si utilizza la keyword loop al posto del while e non si mette nessuna condizione nel seguente modo: 
         loop
         {
             
@@ -28,6 +34,10 @@ fn main() {
     println!("{}", Somma(n, 4));//Stampa il risultato che ritorna la variabile Somma avendo come parametri n(Che ora è pari a 100) e 4: stamperà quindi 104
     Ciao();//Stampa "CIAO!"
     Array();
+    Stringhe();
+
+    let (somma, moltiplicazione) = Calculate(4, 6);//Dichiara due variabile e memorizza al loro interno
+    //I due rispettivamente della somma e della moltiplicazione dati dalla funzione Calculate
     
 
 }
@@ -43,6 +53,10 @@ fn Ciao()//Per una funzione di tipo void basta non specificare il tipo di valore
     println!("CIAO!");
 }
 
+fn Calculate(n1: i32, n2: i32) -> (i32, i32) //Richiede due parametri e dà in output due valori
+{
+    return (n1+n2, n1*n2) //Ritorna sia la somma che la moltipliazione dei due numeri
+}
 
 //Da qui in poi vi saranno solo dei metodi. Per ogni argomento ci sarà un metodo: lo faccio per dare un senso di ordine al tutto
 
@@ -62,4 +76,25 @@ fn Array()
     {
         println!("{i}");//Stampa da 1 a 3
     }
+}
+
+//Ownership: regole per governare la memoria
+//Heap: spazio di memoria dove i dati vengono allocati sennza un ordine preciso. Cerca semplicemente uno spazio libero e
+//      vede se è sufficiente, quindi memorizza il dato e ritorna il puntatore, ovvero l'indirizzo di dove si trova il dato
+//Stack: Tipo di memoria in qui i dati vengono inseriti in modalità LIFO(Last In First Out). E' più veloce l'inserimento di 
+//       dati in questo tipo di memoria poiché non si fa una ricerca di uno spazio di memoria libero, ma si posiziona 
+//       semplicemente il dato in cima allo stack
+
+fn Stringhe()
+{
+    //Stringhe immutabili
+    //Tipo che gestisce i dati allocati nell'heap
+    /*Ci permette di memorizzare un quantitativo di testo a noi sconosciuto:
+    Utile ad esempio quando chiediamo un valore in input da un utente
+    */
+
+    let stringa1: String = String::from("CIAO"); //Variabile stringa con dentro "CIAO"
+    let stringa2: String = stringa1.clone() + &String::from(" Mondo"); // Copiamo la prima stringa nella seconda e gli aggiungiamo mondo
+    println!("{stringa1}");
+    println!("{stringa2}"); 
 }
